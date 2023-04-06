378ad7a9fbede7fb5f088fa204aa4e0e7b0be7b2
Patrick Christie <patrick.christie.dev@gmail.com>
2023-04-06 09:37:09 +0930

    Fixed more stuff

--- frontend-mentor/faq-accordion-card/src/components/FAQCard.jsx
+++ frontend-mentor/faq-accordion-card/src/components/FAQCard.jsx
@@ -3,43 +3,41 @@ import "./FAQCard.css";
 
 const FAQCard = ({ accordionData }) => {
   return (
-    <div className="container">
-      <article className="faq-card">
+    <article className="faq-card">
+      <img
+        className="faq-card__image mobile"
+        src="/images/illustration-woman-online-mobile.svg"
+        alt="Icon"
+      />
+      <img
+        src="/images/illustration-box-desktop.svg"
+        alt=""
+        className="faq-card__image-extra desktop"
+      />
+      <div className="desktop-images">
         <img
-          className="faq-card__image mobile"
-          src="/images/illustration-woman-online-mobile.svg"
+          className="faq-card__image desktop desktop-image"
+          src="/images/illustration-woman-online-desktop.svg"
           alt="Icon"
         />
-        <img
-          src="/images/illustration-box-desktop.svg"
-          alt=""
-          className="faq-card__image-extra desktop"
-        />
-        <div className="desktop-images">
-          <img
-            className="faq-card__image desktop desktop-image"
-            src="/images/illustration-woman-online-desktop.svg"
-            alt="Icon"
-          />
-        </div>
+      </div>
 
-        <div className="faq-card__content">
-          <h1 className="faq-card__header">FAQ</h1>
-          <div className="faq-card__accordion-menu">
-            {accordionData.map((acc) => {
-              return (
-                <Accordion
-                  key={acc.title}
-                  title={acc.title}
-                  content={acc.content}
-                  onClick={() => console.log(`Clicked ${acc.title}`)}
-                />
-              );
-            })}
-          </div>
+      <div className="faq-card__content">
+        <h1 className="faq-card__header">FAQ</h1>
+        <div className="faq-card__accordion-menu">
+          {accordionData.map((acc) => {
+            return (
+              <Accordion
+                key={acc.title}
+                title={acc.title}
+                content={acc.content}
+                onClick={() => console.log(`Clicked ${acc.title}`)}
+              />
+            );
+          })}
         </div>
-      </article>
-    </div>
+      </div>
+    </article>
   );
 };
 
